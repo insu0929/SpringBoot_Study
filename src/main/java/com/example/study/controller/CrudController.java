62601550da9d183c61342c0467fc4e9a61cd1221
insu0929 <insu0929@naver.com>
2021-03-26 15:23:58 +0900

    REFACTORING : Partner abstraction fixed

--- src/main/java/com/example/study/controller/CrudController.java
+++ src/main/java/com/example/study/controller/CrudController.java
@@ -2,10 +2,17 @@ package com.example.study.controller;
 import com.example.study.ifs.CrudInterface;
 import com.example.study.model.network.Header;
 import com.example.study.service.BaseService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@Slf4j
 @Component
 public abstract class CrudController<Req,Res,Entity> implements CrudInterface<Req,Res> {
 
@@ -35,4 +42,12 @@ public abstract class CrudController<Req,Res,Entity> implements CrudInterface<Re
     public Header delete(@PathVariable Long id) {
         return baseService.delete(id);
     }
+
+    @GetMapping("")
+    public Header<List<Res>> search(@PageableDefault(sort = "id", direction = Sort.Direction.ASC, size = 15) Pageable pageable){
+        log.info("{}", pageable);
+        return null;
+        //return baseService.search(pageable);
+    }
+
 }
